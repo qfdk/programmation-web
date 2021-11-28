772980f78b6c6e1f8f9e525dcf76896f1c5d50bf
LI Haozhi <haozhi.li@efrei.fr>
2021-11-28 16:06:47 +0100

    fix: WIP

---
+++ POO/normal/app.js
@@ -0,0 +1,18 @@
+// 我开车去北京的例子
+// 面向过程的
+
+
+console.log("我拿车");
+console.log("我拿钥匙开车门");
+console.log("我发动车子");
+console.log("我开始准备导航");
+console.log("我看看车还有油么");
+// ....
+
+console.log("我上了高速");
+console.log("高速休息");
+console.log("迷路了");
+console.log("重新导航");
+console.log("。。。。");
+console.log("车胎爆了");
+console.log("保险公司。。。");
---
+++ javascript/app.js
@@ -0,0 +1,5 @@
+const obj = {
+    name: "liu"
+};
+
+console.log(obj);
