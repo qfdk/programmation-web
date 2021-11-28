7ec6c9f03394de7f4d03653976754c6a63261772
TianQ9 <qit0108@gmail.com>
2021-11-28 17:55:51 +0100

    fix: 分离成两个文件

---
+++ html/application.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $('#myButton').on('click', () => {
+        var inputValue = $('#myInput').val();
+        if (inputValue.length > 0) {
+            alert(inputValue);
+        }
+
+    });
+    $('#myInput').on('change', () => {
+    });
+    for (let i = 0; i < 10; i++) {
+        $('#myUl').append(`<li>测试</li>`);
+    }
+
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; //含最大值，含最小值 
+
+    }
+    $('#random').on('click', () => {
+        alert(getRandomIntInclusive(1, 5));
+    });
+
+});
