b65227062738133c4693b87e912de64d97a3f4b9
LI Haozhi <haozhi.li@efrei.fr>
2021-11-28 16:05:13 +0100

    fix: WIP

--- html/README.md
+++ html/README.md
@@ -2,3 +2,11 @@
 
 ## vsc 插件下载
 https://marketplace.visualstudio.com/items?itemName=wcwhitehead.bootstrap-3-snippets
+
+### jQuery 
+```javascript
+<!-- jQuery 引入--> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+```
+
+### 所有的 js 代码 放在第`三方库`的后面
--- javascript/html/app.js
+++ javascript/html/app.js
@@ -1,4 +1,5 @@
 const tab = [9, 5, 1, 5];
+alert("我运行了")
 const helloWorld = () => {
     alert("确定删除么？");
     // console.log(tab);
@@ -13,5 +14,4 @@ document.write('Pour se dire aurevoir');
 // helloWorld();
 
 var mondiv = document.getElementById("container");
-console.log(mondiv)
 mondiv.style.backgroundColor = "#fea360";
