ae538b6644967949b978221b9ad5f716669b0379
TianQ9 <qit0108@gmail.com>
2021-12-18 18:41:16 +0100

    swap 交换a,b

--- php/cours1/tp/index.php
+++ php/cours1/tp/index.php
@@ -3,3 +3,15 @@
 for($i=0;$i<10;$i++){
     echo $i;
 }
+
+$a1 = 1;
+$b2 = 2;
+
+function swap(&$a, &$b){
+    $c = $a;
+    $a = $b;
+    $b = $c;
+}
+    echo "<br/>$a1 $b2";
+    swap($a1, $b2);
+    echo "<br/>$a1 $b2";
