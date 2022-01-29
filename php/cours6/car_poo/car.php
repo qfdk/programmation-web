56b03721def5b562c47e5308a7e87e8ab8e98997
TianQ9 <qit0108@gmail.com>
2022-01-29 18:05:47 +0100

    fix : new_car

--- php/cours6/car_poo/car.php
+++ php/cours6/car_poo/car.php
@@ -1,51 +1,64 @@
 <?php
 
-class Car{
+class Car
+{
 
     protected $color;
     protected $marque;
 
-    public function __construct($color ="black", $marque="")
+    public function __construct($color = "black", $marque = "")
     {
-        echo"2.".$color;
+        echo "2. car: " . $color . "<br>";
         $this->color = $color;
         $this->marque = $marque;
     }
 
 
-    public function go($lieu){
-        echo $this->marque."去".$lieu; 
+    public function go($lieu)
+    {
+        echo $this->marque . "去" . $lieu;
     }
 
-
+    public function __toString()
+    {
+        echo "car 里的 string :<br>";
+        return "
+               color ：$this->color<br>
+               marque：$this->marque";
+    }
 }
 
 
-class Audi extends Car{
+class Audi extends Car
+{
 
     private $id;
 
     public function __construct($color, $marque, $id)
     {
-        echo"1.".$color;
-        parent::__construct($color,$marque);
+        echo "1.audi car: " . $color . "<br>";
+        parent::__construct($color, $marque);
         $this->id = $id;
     }
 
-    public function go($lieu){
-        echo $this->id."去".$lieu; 
+    public function go($lieu)
+    {
+        echo $this->id . "去" . $lieu;
     }
 
     public function __toString()
     {
-   
-        return "$this->color<br>
-               $this->marque<br>
-               $this->id";
-   
-    }       
+        $ptosting = parent::__toString();
+        return "audi 里的 string :
+                        <br>$ptosting 
+                        <br> id :   $this->id";
+    }
 }
 
 
-$audi=new Audi("red","audi","奥迪车");
+
+
+$audi = new Audi("red", "audi", "奥迪车");
 echo $audi;
+// $car = new Car();
+// echo $car;
